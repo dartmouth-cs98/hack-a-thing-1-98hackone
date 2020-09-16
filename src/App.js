72105d20f86e6d841794fe217ffded80ce1c46e8
Kyle <ziemerjkyle@gmail.com>
2020-09-16 12:11:35 -0500

    First commit with my first working hello world in web development!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// here I deleted some stuff from the original page and just put in hello world!
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">    
+        Hello world!
       </header>
     </div>
   );
